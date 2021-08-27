51f8413c7b2a766b7a0719ca9c25d2c18343f2b5
DezlLab <dezl00@gmx.net>
2021-08-27 17:05:16 +0200

    Now :]

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  var test : string = "Abc"
+  var abc : string = "abc"
+  if(test === abc){
+    console.log("Nice")
+  }
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
